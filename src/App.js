dc49b07b813f992da78205e00347068aaa36504d
zeriaaron <zeriaaron23@gmail.com>
2024-04-14 07:24:09 +0800

    Edited the path of Link

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ function App() {
                 <h1>Zeri Aaron Malacas</h1>
                 <nav>
                     <ul>
-                        <li><Link to={'/home'} className='nav-item'>Home</Link></li>
-                        <li><Link to={'/about'} className='nav-item'>About</Link></li>
-                        <li><Link to={'/experience'} className='nav-item'>Experience</Link></li>
-                        <li><Link to={'/projects'} className='nav-item'>Projects</Link></li>
+                        <li><Link to={'home'} className='nav-item'>Home</Link></li>
+                        <li><Link to={'about'} className='nav-item'>About</Link></li>
+                        <li><Link to={'experience'} className='nav-item'>Experience</Link></li>
+                        <li><Link to={'projects'} className='nav-item'>Projects</Link></li>
                     </ul>
                 </nav>
 
                 {/* Routing */}
                 <Routes>
-                    <Route path='/home' element={<Home />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/experience' element={<Experience />} />
-                    <Route path='/projects' element={<Projects />} />
+                    <Route path='home' element={<Home />} />
+                    <Route path='about' element={<About />} />
+                    <Route path='experience' element={<Experience />} />
+                    <Route path='projects' element={<Projects />} />1
                 </Routes>
             </header>
 
