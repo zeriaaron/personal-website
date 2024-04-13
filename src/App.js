607f57b7d56ce830c0bf29f6cac35a6886bf4e7c
zeriaaron <zeriaaron23@gmail.com>
2024-04-14 07:51:24 +0800

    Edited the right routing

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ function App() {
                 <h1>Zeri Aaron Malacas</h1>
                 <nav>
                     <ul>
-                        <li><Link to={'home'} className='nav-item'>Home</Link></li>
-                        <li><Link to={'about'} className='nav-item'>About</Link></li>
-                        <li><Link to={'experience'} className='nav-item'>Experience</Link></li>
-                        <li><Link to={'projects'} className='nav-item'>Projects</Link></li>
+                        <li><Link to={'personal-website/'} className='nav-item'>Home</Link></li>
+                        <li><Link to={'personal-website/about'} className='nav-item'>About</Link></li>
+                        <li><Link to={'personal-website/experience'} className='nav-item'>Experience</Link></li>
+                        <li><Link to={'personal-website/projects'} className='nav-item'>Projects</Link></li>
                     </ul>
                 </nav>
 
                 {/* Routing */}
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='about' element={<About />} />
-                    <Route path='experience' element={<Experience />} />
-                    <Route path='projects' element={<Projects />} />
+                    <Route path='personal-website/' element={<Home />} />
+                    <Route path='personal-website/about' element={<About />} />
+                    <Route path='personal-website/experience' element={<Experience />} />
+                    <Route path='personal-website/projects' element={<Projects />} />
                 </Routes>
             </header>
 
