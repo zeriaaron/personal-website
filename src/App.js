41090bd5460d43a208f415f068c93bf570052439
zeriaaron <zeriaaron23@gmail.com>
2024-04-14 15:53:09 +0800

    Updated the nav of App

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import './App.css';
 import {Routes, Route, Link} from 'react-router-dom'
 import {Home, About, Experience, Projects} from './components';
 
+// https://zeriaaron.github.io/personal-website [root: zeriaaron.github.io]
+// root-relative path
+// /personal-website/about -> https://zeriaaron.github.io/personal-website/about
+
 function App() {
     return (
-        <>
+        <>  
             {/* Unchangeable */}
             <header>
-                <h1>Zeri Aaron Malacas</h1>
-                <nav>
-                    <ul>
-                        <li><Link to={'personal-website/'} className='nav-item'>Home</Link></li>
-                        <li><Link to={'personal-website/about'} className='nav-item'>About</Link></li>
-                        <li><Link to={'personal-website/experience'} className='nav-item'>Experience</Link></li>
-                        <li><Link to={'personal-website/projects'} className='nav-item'>Projects</Link></li>
-                    </ul>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to={'/'}>Zeri Aaron Malacas</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <Link to={'/'} className='nav-link' aria-current="page">Home</Link>
+                                <Link to={'/about'} className='nav-link'>About</Link>
+                                <Link to={'/experience'} className='nav-link'>Experience</Link>
+                                <Link to={'/projects'} className='nav-link'>Projects</Link>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
 
                 {/* Routing */}
                 <Routes>
-                    <Route path='personal-website/' element={<Home />} />
-                    <Route path='personal-website/about' element={<About />} />
-                    <Route path='personal-website/experience' element={<Experience />} />
-                    <Route path='personal-website/projects' element={<Projects />} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/experience' element={<Experience />} />
+                    <Route path='/projects' element={<Projects />} />
                 </Routes>
             </header>
 
