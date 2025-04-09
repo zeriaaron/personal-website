cb1d560fadb49a02980127b4ce064e893b8231ed
zeriaaron <zeriaaron23@gmail.com>
2025-04-09 18:04:07 +0800

    Removed the routing and moved to App

--- src/components/parts/Header.js
+++ src/components/parts/Header.js
@@ -1,6 +1,5 @@
-import { Routes, Route, Navigate, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useRef, useState } from 'react'
-import { Home, About, Certificates, Projects, Contacts } from '../../components'
 import headerStyle from '../../assets/styles/parts/Header.module.css'
 
 
@@ -8,28 +7,6 @@ import headerStyle from '../../assets/styles/parts/Header.module.css'
 export default function Header() {
     const togglerIcon = useRef(null)
     const [isClicked, setIsClicked] = useState(false)
-
-    const links = {
-        home: {
-            content: 'Home'
-        },
-        about: {
-            link: 'about',
-            content: 'About'
-        },
-        certificates: {
-            link: 'certificates',
-            content: 'Certificates'
-        },
-        projects: {
-            link: 'projects',
-            content: 'Projects'
-        },
-        contacts: {
-            link: 'contacts',
-            content: 'Contacts'
-        },
-    }
 
     let clickHandler = () => {
         if (isClicked) {
@@ -62,26 +39,15 @@ export default function Header() {
                     {/* Home About Certificates Projects Contacts */}
                     <div className={`collapse navbar-collapse ${headerStyle.navbarCollapse}`} id="navbarNavAltMarkup">
                         <div className={`navbar-nav ${headerStyle.navbarNav}`}>
-                            <NavLink to={'/personal-website/'} end className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`} aria-current="page">{links.home.content}</NavLink>
-                            <NavLink to={`/personal-website/${links.about.link}`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>{links.about.content}</NavLink>
-                            <NavLink to={`/personal-website/${links.certificates.link}`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>{links.certificates.content}</NavLink>
-                            <NavLink to={`/personal-website/${links.projects.link}`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>{links.projects.content}</NavLink>
-                            <NavLink to={`/personal-website/${links.contacts.link}`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>{links.contacts.content}</NavLink>
+                            <NavLink to={`/home`} end className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`} aria-current="page">Home</NavLink>
+                            <NavLink to={`/about`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>About</NavLink>
+                            <NavLink to={`/certificates`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>Certificates</NavLink>
+                            <NavLink to={`/projects`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>Projects</NavLink>
+                            <NavLink to={`/contacts`} className={({ isActive }) => `nav-link ${headerStyle.navLink} ${isActive ? headerStyle.activeLink : ''}`}>Contacts</NavLink>
                         </div>
                     </div>
                 </div>
             </nav>
-
-            {/* Routing */}
-            <Routes>
-                <Route path='/personal-website/' element={<Home />} />
-                <Route path='/personal-website/about' element={<About />} />
-                <Route path='/personal-website/certificates' element={<Certificates />} />
-                <Route path='/personal-website/projects' element={<Projects />} />
-                <Route path='/personal-website/contacts' element={<Contacts />} />
-
-                <Route path='*' element={<Navigate to='/personal-website/' />} />
-            </Routes>
         </header >
     )
 }
